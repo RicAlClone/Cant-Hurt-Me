5a06ce90948352f77fddc54a0b13b190cf27eb54
rickyalatorre <alatorre1160@yahoo.com>
2022-04-13 09:58:06 -0700

    fix ./client/build and 'server'

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const path = require("path");
   app.use(express.static(path.resolve(__dirname,'./client/build')));
 
   app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./client","index.html"));
+    res.sendFile(path.resolve(__dirname,"./client/build","index.html"));
   });
 
 
